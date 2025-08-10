d3f8b3fe01af556a392053def91aa6b705c0f754
gianpierreSO <gianpierrec712@gmail.com>
2025-08-09 22:20:44 -0500

    funcion actualizar y sortear amigos

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-function asignarTextoElemento(elemento,texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
-
 function agregarAmigo(){
     let nuevoAmigo = document.getElementById("amigo").value;
     if(nuevoAmigo=='' || nuevoAmigo==""){
@@ -14,10 +8,35 @@ function agregarAmigo(){
     else{
         amigos.push(nuevoAmigo)
         document.querySelector('#amigo').value = '';
+        actualizarAmigos()
     }
+    
+    return;
 }
 
+function actualizarAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpiar contenido anterior
+    // Recorrer el array y agregar cada amigo como <li>
+    let listaHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        listaHTML += `<li>${amigos[i]}</li>`;
+    }
+    lista.innerHTML = listaHTML;
+}
 
+function sortearAmigo() {
+    if(amigos.length===0){
+        alert("Agrega amigos para sortear");
+        return;
+    }
+    
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>Tu amigo secreto es: <strong>${amigoSorteado}</strong></li>`;
+    
+}
 
 
 let amigos = []
