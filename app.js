dd1d1f0b85807584d580d4dc0d51be7a1ef10e00
gianpierreSO <gianpierrec712@gmail.com>
2025-08-09 21:19:08 -0500

    funcion agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+function asignarTextoElemento(elemento,texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo(){
+    let nuevoAmigo = document.getElementById("amigo").value;
+    if(nuevoAmigo=='' || nuevoAmigo==""){
+        alert("Por favor, inserte un nombre")
+    }
+    else{
+        amigos.push(nuevoAmigo)
+        document.querySelector('#amigo').value = '';
+    }
+}
+
+
+
+
+let amigos = []
+
